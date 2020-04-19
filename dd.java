f8e5060d50dd7e68e094db3c40cebea01cf56f47
Shoumen Nath1 <Shoumen.Nath1@synechron.com>
2020-04-19 17:43:21 +0530

    Adding new files

---
+++ dd.java
@@ -0,0 +1,10 @@
+package com.syn.test.demotest;
+
+public class dd {
+
+	public static void main(String[] args) {
+		
+System.out.println("Jenkins World");
+	}
+
+}
---
+++ demo.java
@@ -0,0 +1,5 @@
+package com.syn.test.demotest;
+
+public class demo {
+
+}
